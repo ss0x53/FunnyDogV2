4a3f3bff6b341a96526df60944020c85773fb9e4
ss0x53 <ss0x53@outlook.com>
2014-09-03 20:52:42 +0800

    Change something

--- Assets/Code/Logic/UI/Initer/ArrowGridIniter.cs
+++ Assets/Code/Logic/UI/Initer/ArrowGridIniter.cs
@@ -12,6 +12,7 @@ public class ArrowGridIniter : MonoBehaviour {
         for (int i = 0; i < number; ++i)
         {
             GameObject go = GlobalManager.Instance.GetAssetsManager.GetGameObject("Grid");
+            grids.Add(go);
         }
         ReposAllGrids();
     }
